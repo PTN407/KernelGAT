7c19e5908a95b3cee972a677be478e13d24a2610
Phan Trọng Nhân <92132592+PTN407@users.noreply.github.com>
2023-10-31 10:09:12 +0700

    Update train.py

--- kgat/train.py
+++ kgat/train.py
@@ -85,14 +85,15 @@ def train_model(model, ori_model, args, trainset_reader, validset_reader):
                     print('Dev total acc: {0}'.format(dev_accuracy))
             if global_step % (args.eval_step * args.gradient_accumulation_steps) == 0:
                 logger.info('Start eval!')
-                eval_acc = eval_model(model, validset_reader)
-                logger.info('Dev acc: {0}'.format(eval_acc))
-                if eval_acc >= best_accuracy:
-                    best_accuracy = eval_acc
-                    torch.save({'epoch': epoch,
-                                'model': ori_model.state_dict()}, save_path + ".best.pt")
-                    logger.info("Saved best epoch {0}, best acc {1}".format(epoch, best_accuracy))
-                    print("Saved best epoch {0}, best acc {1}".format(epoch, best_accuracy))
+                with torch.no_grad():
+                    eval_acc = eval_model(model, validset_reader)
+                    logger.info('Dev acc: {0}'.format(eval_acc))
+                    if eval_acc >= best_accuracy:
+                        best_accuracy = eval_acc
+                        torch.save({'epoch': epoch,
+                                    'model': ori_model.state_dict()}, save_path + ".best.pt")
+                        logger.info("Saved best epoch {0}, best acc {1}".format(epoch, best_accuracy))
+                        print("Saved best epoch {0}, best acc {1}".format(epoch, best_accuracy))
 
 
 if __name__ == "__main__":
